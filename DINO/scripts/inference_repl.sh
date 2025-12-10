bad60bea67ec6be20c688be5db37da2d428d8384
Jing Wu <jingwusince01@gmail.com>
2025-12-10 15:08:01 +0800

    add inference option batch size

--- DINO/scripts/inference_repl.sh
+++ DINO/scripts/inference_repl.sh
@@ -5,14 +5,14 @@ python inference_5scale.py \
     --input_folder data/testing_image \
     --output_file $1 \
     --confidence_threshold 0.15 \
-    --batch_size 64 \
+    --batch_size $2 \
     --num_workers 2
 
 python inference_4scale.py \
     --config_file config/DINO/DINO_valve_4scale.py \
     --checkpoint_path checkpoints/checkpoint_4scale_24epochs.pth \
     --input_folder data/testing_image \
-    --output_file $2 \
+    --output_file $3 \
     --confidence_threshold 0.15 \
-    --batch_size 64 \
+    --batch_size $4 \
     --num_workers 2
